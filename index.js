26e6e730ab9304c9339c764dacc896c08634faa1
Fede <fambroggio.polko@gmail.com>
2023-01-07 17:54:14 -0300

    Routes and methods added

--- index.js
+++ index.js
@@ -1,14 +1,59 @@
 const express = require("express");
 const app = express();
-const router = express.Router();
+const routerProductos = express.Router();
+const routerCarrito = express.Router();
 const fs = require("fs");
 
+
+
+routerProductos.route('/')
+    .get((req, res) => {
+        res.send({success: 'lista de todos los productos'})
+    })
+    .post((req, res) => {
+        res.send({success: `Producto ${JSON.stringify(req.body)} añadido`})
+    })
+
+routerProductos.route('/:id')
+    .get((req, res) => {
+        res.send({success: `producto ${req.params.id}`})
+    })
+    .put((req, res) => {
+        res.send({success: `actualizar ${req.params.id}`})
+    })
+    .delete((req, res) => {
+        res.send({success: `borrar ${req.params.id}`})
+    })
+
+routerCarrito.route('/')
+    .post((req, res) => {
+        res.send({success: `Carrito ${JSON.stringify(req.body)} añadido`})
+    })
+
+routerCarrito.route('/:id')
+    .delete((req, res) => {
+        res.send({success: `borrar ${req.params.id}`})
+    })
+
+routerCarrito.route('/:id/productos')
+    .get((req, res) => {
+        res.send({success: `producto ${req.params.id}`})
+    })
+    .post((req, res) => {
+        res.send({success: `agregar ${req.params.id}`})
+    })
+
+routerCarrito.route('/:id/productos/:id_prod')
+    .delete((req, res) => {
+        res.send({success: `borrar ${req.params.id_prod} de ${req.params.id}`})
+    })
 
 // SERVER CONFIG
 app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 // app.use(express.static('public'));
-app.use('/', router);
+app.use('/api/productos', routerProductos);
+app.use('/api/carrito', routerCarrito);
 
 // SERVER INIT
 const server = app.listen(8080, () => {
